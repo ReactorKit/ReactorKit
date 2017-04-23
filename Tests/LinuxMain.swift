48fa529f67e9045348f1f39f87d85d7d4a523fd8
Suyeol Jeon <devxoul@gmail.com>
2017-04-24 02:18:22 +0900

    Remove linux test code

--- Tests/LinuxMain.swift
+++
@@ -1,6 +0,0 @@
-import XCTest
-@testable import ReactorTests
-
-XCTMain([
-  testCase(ReactorTests.allTests),
-])
--- Tests/ReactorTests/ReactorTests.swift
+++ Tests/ReactorTests/ReactorTests.swift
@@ -2,14 +2,4 @@ import XCTest
 @testable import ReactorKit
 
 class ReactorKitTests: XCTestCase {
-  func testExample() {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-    XCTAssertEqual(ReactorKit().text, "Hello, World!")
-  }
-
-
-  static var allTests = [
-    ("testExample", testExample),
-  ]
 }
