671e5d51d3e077d2f930ab5bbefa2861427f6f19
Suyeol Jeon <devxoul@gmail.com>
2017-04-27 04:06:53 +0900

    Add RxExpect as a dependency

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 let package = Package(
   name: "ReactorKit",
   dependencies: [
-    .Package(url: "https://github.com/ReactiveX/RxSwift.git", majorVersion: 3),
+    .Package(url: "https://github.com/devxoul/RxSwift.git", majorVersion: 3),
+    .Package(url: "https://github.com/devxoul/RxExpect.git", majorVersion: 0),
   ]
 )
