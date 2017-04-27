2d0b20baae413e628fb996a34c164f710f9ba48e
Suyeol Jeon <devxoul@gmail.com>
2017-04-27 17:36:41 +0900

    Add some more test cases

--- Tests/ReactorKitTests/ReactorTests.swift
+++ Tests/ReactorKitTests/ReactorTests.swift
@@ -11,8 +11,7 @@ final class ReactorTests: XCTestCase {
       test.input(reactor.action, [
         next(100, ["action"]),
       ])
-      test
-        .assert(reactor.state)
+      test.assert(reactor.state)
         .filterNext()
         .equal([
           ["transformedState"], // initial state
@@ -20,6 +19,57 @@ final class ReactorTests: XCTestCase {
         ])
     }
   }
+
+  func testCurrentState() {
+    let disposeBag = DisposeBag()
+    let reactor = TestReactor()
+    XCTAssertEqual(reactor.currentState, [])
+    reactor.state.subscribe().disposed(by: disposeBag)
+    reactor.action.onNext(["action"])
+    XCTAssertEqual(reactor.currentState, ["action", "transformedAction", "mutation", "transformedMutation", "transformedState"])
+  }
+
+  func testStreamNotContainsError() {
+    RxExpect { test in
+      let reactor = TestReactor()
+      reactor.shouldEmitErrorOnMutate = true
+      test.input(reactor.action, [
+        next(100, ["action"]),
+      ])
+      test.assert(reactor.state)
+        .not()
+        .since(100)
+        .contains { event in
+          if case .error = event.value {
+            return true
+          } else {
+            return false
+          }
+        }
+    }
+  }
+
+  func testStreamNotContainsCompleted() {
+    RxExpect { test in
+      let reactor = TestReactor()
+      reactor.shouldCompleteOnMutate = true
+      test.input(reactor.action, [
+        next(100, ["action"]),
+      ])
+      test.assert(reactor.state)
+        .not()
+        .contains { event in
+          if case .completed = event.value {
+            return true
+          } else {
+            return false
+          }
+        }
+    }
+  }
+}
+
+struct TestError: Error {
 }
 
 private final class TestReactor: Reactor {
@@ -28,6 +78,8 @@ private final class TestReactor: Reactor {
   typealias State = [String]
 
   let initialState = State()
+  var shouldEmitErrorOnMutate = false
+  var shouldCompleteOnMutate = false
 
   // 1. ["action"] + ["transformedAction"]
   func transform(action: Observable<Action>) -> Observable<Action> {
@@ -36,7 +88,13 @@ private final class TestReactor: Reactor {
 
   // 2. ["action", "transformedAction"] + ["mutation"]
   func mutate(action: Action) -> Observable<Mutation> {
-    return .just(action + ["mutation"])
+    if self.shouldEmitErrorOnMutate {
+      return .error(TestError())
+    } else if self.shouldCompleteOnMutate {
+      return .empty()
+    } else {
+      return .just(action + ["mutation"])
+    }
   }
 
   // 3. ["action", "transformedAction", "mutation"] + ["transformedMutation"]
