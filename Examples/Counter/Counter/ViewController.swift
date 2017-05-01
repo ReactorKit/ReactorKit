589eb94790026925f4825ce9abe688278e940805
Suyeol Jeon <devxoul@gmail.com>
2017-05-02 00:45:02 +0900

    Add CounterExample

---
+++ Examples/Counter/Counter/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Suyeol Jeon on 02/05/2017.
+//  Copyright © 2017 Suyeol Jeon. All rights reserved.
+//
+
+import UIKit
+
+import ReactorKit
+import RxCocoa
+import RxSwift
+
+final class CounterViewController: UIViewController, View {
+  @IBOutlet var decreaseButton: UIButton!
+  @IBOutlet var increaseButton: UIButton!
+  @IBOutlet var valueLabel: UILabel!
+  var disposeBag = DisposeBag()
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    reactor = CounterViewReactor() // this makes `bind(reactor:)` gets called
+  }
+
+  // Called when the new value is assigned to `self.reactor`
+  func bind(reactor: CounterViewReactor) {
+    // Action
+    increaseButton.rx.tap               // Tap event
+      .map { Reactor.Action.increase }  // Convert to Action.increase
+      .bind(to: reactor.action)         // Bind to reactor.action
+      .disposed(by: disposeBag)
+
+    decreaseButton.rx.tap
+      .map { Reactor.Action.decrease }
+      .bind(to: reactor.action)
+      .disposed(by: disposeBag)
+
+    // State
+    reactor.state                    // State(value: 10)
+      .map { $0.value }              // 10
+      .map { "\($0)" }               // "10"
+      .bind(to: valueLabel.rx.text)  // Bind to valueLabel
+      .disposed(by: disposeBag)
+  }
+}
+
+final class CounterViewReactor: Reactor {
+  // Action is an user interaction
+  enum Action {
+    case increase
+    case decrease
+  }
+
+  // Mutate is a state manipulator
+  enum Mutation {
+    case increaseValue
+    case decreaseValue
+  }
+
+  // State is a current view state
+  struct State {
+    var value: Int
+  }
+
+  let initialState = State(value: 0) // start from 0
+
+  // Action -> Mutation
+  func mutate(action: Action) -> Observable<Mutation> {
+    switch action {
+    case .increase:
+      return Observable.just(Mutation.increaseValue)
+    case .decrease:
+      return Observable.just(Mutation.decreaseValue)
+    }
+  }
+
+  // Mutation -> State
+  func reduce(state: State, mutation: Mutation) -> State {
+    switch mutation {
+    case .increaseValue:
+      return State(value: state.value + 1)
+    case .decreaseValue:
+      return State(value: state.value - 1)
+    }
+  }
+}
---
+++ Examples/Counter/README.md
@@ -0,0 +1,12 @@
+# Counter
+
+Counter application is the most simple and the basic example of ReactorKit.
+
+![counter](https://cloud.githubusercontent.com/assets/931655/25584276/47df107c-2ed0-11e7-9ca3-4d1407259e2d.png)
+
+To run the project, use:
+
+```console
+$ pod install
+$ open Counter.xcworkspace
+```
