2d30f10287d9667604471cc8affb7cd24973d472
Suyeol Jeon <devxoul@gmail.com>
2017-05-13 04:12:15 +0900

    Implement reactor with dummy data

--- Examples/GitHubSearch/GitHubSearch/GitHubSearchViewReactor.swift
+++ Examples/GitHubSearch/GitHubSearch/GitHubSearchViewReactor.swift
@@ -7,16 +7,41 @@
 //
 
 import ReactorKit
+import RxSwift
 
 final class GitHubSearchViewReactor: Reactor {
   enum Action {
+    case updateQuery(String?)
   }
 
   enum Mutation {
+    case setRepos([String])
   }
 
   struct State {
+    var repos: [String] = []
   }
 
   let initialState = State()
+
+  func mutate(action: Action) -> Observable<Mutation> {
+    switch action {
+    case let .updateQuery(query):
+      if let query = query {
+        let dummyRepos = ["\(query)1", "\(query)2", "\(query)3"]
+        return Observable.just(Mutation.setRepos(dummyRepos))
+      } else {
+        return Observable.just(Mutation.setRepos([]))
+      }
+    }
+  }
+
+  func reduce(state: State, mutation: Mutation) -> State {
+    switch mutation {
+    case let .setRepos(repos):
+      var newState = state
+      newState.repos = repos
+      return newState
+    }
+  }
 }
