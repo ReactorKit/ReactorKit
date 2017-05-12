c2e3ecbbd8af43d85192793b009819fffb33b3b8
Suyeol Jeon <devxoul@gmail.com>
2017-05-13 03:58:07 +0900

    Add GitHubSearchViewReactor

---
+++ Examples/GitHubSearch/GitHubSearch/GitHubSearchViewReactor.swift
@@ -0,0 +1,22 @@
+//
+//  GitHubSearchViewReactor.swift
+//  GitHubSearch
+//
+//  Created by Suyeol Jeon on 13/05/2017.
+//  Copyright © 2017 Suyeol Jeon. All rights reserved.
+//
+
+import ReactorKit
+
+final class GitHubSearchViewReactor: Reactor {
+  enum Action {
+  }
+
+  enum Mutation {
+  }
+
+  struct State {
+  }
+
+  let initialState = State()
+}
