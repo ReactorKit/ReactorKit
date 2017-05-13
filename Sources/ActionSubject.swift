ed4f922c801092de67acffe776b098b296029784
Suyeol Jeon <devxoul@gmail.com>
2017-05-14 01:25:28 +0900

    Add ActionSubject

---
+++ Sources/ActionSubject.swift
@@ -0,0 +1,56 @@
+//
+//  ActionSubject.swift
+//  ReactorKit
+//
+//  Created by Suyeol Jeon on 14/05/2017.
+//
+//
+
+import class Foundation.NSLock.NSRecursiveLock
+
+import RxSwift
+
+/// A special subject for Reactor's Action. It only emits `.next` event.
+public class ActionSubject<Element>: ObservableType, ObserverType, SubjectType {
+  public typealias E = Element
+  typealias Key = UInt
+
+  var lock = NSRecursiveLock()
+
+  var nextKey: Key = 0
+  var observers: [Key: (Event<Element>) -> ()] = [:]
+
+  #if TRACE_RESOURCES
+  init() {
+    _ = Resources.incrementTotal()
+  }
+  #endif
+
+  #if TRACE_RESOURCES
+  deinit {
+    _ = Resources.decrementTotal()
+  }
+  #endif
+
+  public func subscribe<O: ObserverType>(_ observer: O) -> Disposable where O.E == Element {
+    self.lock.lock()
+    let key = self.nextKey
+    self.nextKey += 1
+    self.observers[key] = observer.on
+    self.lock.unlock()
+
+    return Disposables.create {
+      self.lock.lock()
+      self.observers.removeValue(forKey: key)
+      self.lock.unlock()
+    }
+  }
+
+  public func on(_ event: Event<Element>) {
+    self.lock.lock()
+    if case .next = event {
+      self.observers.values.forEach { $0(event) }
+    }
+    self.lock.unlock()
+  }
+}
---
+++ Tests/ReactorKitTests/ActionSubjectTests.swift
@@ -0,0 +1,65 @@
+import XCTest
+import RxExpect
+import RxSwift
+import RxTest
+@testable import ReactorKit
+
+final class ActionSubjectTests: XCTestCase {
+  func testObserversCount_disposable() {
+    let subject = ActionSubject<Int>()
+    let disposable1 = subject.subscribe()
+    XCTAssertEqual(subject.observers.count, 1)
+    let disposable2 = subject.subscribe()
+    XCTAssertEqual(subject.observers.count, 2)
+    disposable2.dispose()
+    XCTAssertEqual(subject.observers.count, 1)
+    disposable1.dispose()
+    XCTAssertEqual(subject.observers.count, 0)
+  }
+
+  func testEmitNexts() {
+    RxExpect { test in
+      let subject = ActionSubject<Int>()
+      test.input(subject, [
+        next(100, 1),
+        next(200, 2),
+        next(300, 3),
+      ])
+      test.assert(subject).equal([
+        next(100, 1),
+        next(200, 2),
+        next(300, 3),
+      ])
+    }
+  }
+
+  func testIgnoreError() {
+    RxExpect { test in
+      let subject = ActionSubject<Int>()
+      test.input(subject, [
+        next(100, 1),
+        error(200, TestError()),
+        next(300, 3),
+      ])
+      test.assert(subject).equal([
+        next(100, 1),
+        next(300, 3),
+      ])
+    }
+  }
+
+  func testIgnoreCompleted() {
+    RxExpect { test in
+      let subject = ActionSubject<Int>()
+      test.input(subject, [
+        next(100, 1),
+        completed(200),
+        next(300, 3),
+      ])
+      test.assert(subject).equal([
+        next(100, 1),
+        next(300, 3),
+      ])
+    }
+  }
+}
