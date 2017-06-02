bc519acdc52b1b2a3704cdcfd28a7759d8535d38
Suyeol Jeon <devxoul@gmail.com>
2017-06-03 03:44:54 +0900

    Merge ruby script files into a single file

--- scripts/remove_rxcocoaruntime.rb
+++
@@ -1,8 +0,0 @@
-require "xcodeproj"
-
-project = Xcodeproj::Project.open("ReactorKit.xcodeproj") or exit
-target = project.targets.find { |t| t.name == "ReactorKit" } or exit
-phase = target.build_phases.find { |p| p.kind_of?(Xcodeproj::Project::Object::PBXFrameworksBuildPhase) } or exit
-file = phase.files_references.find { |f| f.path == "RxCocoaRuntime.framework" } or exit
-phase.remove_file_reference(file)
-project.save
