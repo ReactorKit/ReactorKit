3836798d7f636b446e222bfc8259bcb614595be4
Suyeol Jeon <devxoul@gmail.com>
2017-07-02 18:03:57 +0900

    Update a github search example to assign a reactor outside of a view controller

--- Examples/GitHubSearch/GitHubSearch/AppDelegate.swift
+++ Examples/GitHubSearch/GitHubSearch/AppDelegate.swift
@@ -15,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+    let navigationController = self.window?.rootViewController as! UINavigationController
+    let viewController = navigationController.viewControllers.first as! GitHubSearchViewController
+    viewController.reactor = GitHubSearchViewReactor()
     return true
   }
 
