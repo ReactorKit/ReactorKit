9e7709ff8c1b7809dc3c6a21cd4e25493e5d9885
Suyeol Jeon <devxoul@gmail.com>
2017-07-02 17:56:04 +0900

    Update a counter example to assign a reactor outside of a view controller

--- Examples/Counter/Counter/AppDelegate.swift
+++ Examples/Counter/Counter/AppDelegate.swift
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+    let viewController = self.window?.rootViewController as! CounterViewController
+    viewController.reactor = CounterViewReactor()
     return true
   }
 
