eafe402d38189f8d07b513cd0c812856cca81bcc
Suyeol Jeon <devxoul@gmail.com>
2017-09-23 01:31:39 +0900

    Update example projects to Swift 4

--- Examples/GitHubSearch/GitHubSearch/GitHubSearchViewController.swift
+++ Examples/GitHubSearch/GitHubSearch/GitHubSearchViewController.swift
@@ -14,20 +14,29 @@ import RxCocoa
 import RxSwift
 
 class GitHubSearchViewController: UIViewController, StoryboardView {
-  @IBOutlet var searchBar: UISearchBar!
   @IBOutlet var tableView: UITableView!
+  let searchController = UISearchController(searchResultsController: nil)
 
   var disposeBag = DisposeBag()
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    tableView.contentInset.top = 44 // search bar height
     tableView.scrollIndicatorInsets.top = tableView.contentInset.top
+    searchController.dimsBackgroundDuringPresentation = false
+    navigationItem.searchController = searchController
+  }
+
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    UIView.setAnimationsEnabled(false)
+    searchController.isActive = true
+    searchController.isActive = false
+    UIView.setAnimationsEnabled(true)
   }
 
   func bind(reactor: GitHubSearchViewReactor) {
     // Action
-    searchBar.rx.text
+    searchController.searchBar.rx.text
       .throttle(0.3, scheduler: MainScheduler.instance)
       .map { Reactor.Action.updateQuery($0) }
       .bind(to: reactor.action)
@@ -54,11 +63,12 @@ class GitHubSearchViewController: UIViewController, StoryboardView {
     tableView.rx.itemSelected
       .subscribe(onNext: { [weak self, weak reactor] indexPath in
         guard let `self` = self else { return }
+        self.view.endEditing(true)
         self.tableView.deselectRow(at: indexPath, animated: false)
         guard let repo = reactor?.currentState.repos[indexPath.row] else { return }
         guard let url = URL(string: "https://github.com/\(repo)") else { return }
         let viewController = SFSafariViewController(url: url)
-        self.present(viewController, animated: true, completion: nil)
+        self.searchController.present(viewController, animated: true, completion: nil)
       })
       .disposed(by: disposeBag)
   }
