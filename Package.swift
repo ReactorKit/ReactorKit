f3fd9e46dff64fca8f4fb8842bc3996b15b5c794
Suyeol Jeon <devxoul@gmail.com>
2017-10-11 14:09:36 +0900

    Update RxSwift dependency to 4.0.0-rc.0

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     .library(name: "ReactorKit", targets: ["ReactorKit"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/ReactiveX/RxSwift.git", .branch("rxswift4.0-swift4.0")),
+    .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "4.0.0-rc.0")),
     .package(url: "https://github.com/devxoul/RxExpect.git", .branch("swift-4.0"))
   ],
   targets: [
