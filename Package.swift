03c4751b578e031da3094f77fc8d7ba2eb22a9e3
Suyeol Jeon <devxoul@gmail.com>
2017-10-19 01:00:23 +0900

    Update RxSwift dependency to 4.0.0

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     .library(name: "ReactorKit", targets: ["ReactorKit"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "4.0.0-rc.0")),
     .package(url: "https://github.com/devxoul/RxExpect.git", .branch("swift-4.0"))
+    .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "4.0.0")),
   ],
   targets: [
     .target(name: "ReactorKit", dependencies: ["ReactorKitRuntime", "RxSwift"]),
--- ReactorKit.podspec
+++ ReactorKit.podspec
@@ -9,7 +9,7 @@ Pod::Spec.new do |s|
                          :tag => s.version.to_s }
   s.source_files = "Sources/**/*.{swift,h,m}"
   s.frameworks   = "Foundation"
-  s.dependency "RxSwift", ">= 3.0.0"
+  s.dependency "RxSwift", ">= 4.0.0"
 
   s.ios.deployment_target = "8.0"
   s.osx.deployment_target = "10.11"
