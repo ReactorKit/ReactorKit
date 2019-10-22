6f319086f16739e8a1ae70d90795fddab223bc0f
Mario Hahn <mario_hahn@me.com>
2019-10-22 11:22:08 +0200

    fix catalyst bug

--- Sources/ReactorKitRuntime/ReactorKitRuntime.m
+++ Sources/ReactorKitRuntime/ReactorKitRuntime.m
@@ -7,7 +7,9 @@
   static dispatch_once_t onceToken;
   dispatch_once(&onceToken, ^{
     [self swizzleInitializeOfClassNamed:@"UIViewController"];
+    #if !TARGET_OS_MACCATALYST
     [self swizzleInitializeOfClassNamed:@"NSViewController"];
+    #endif
   });
 }
 
